2d8ff18a88c45f03e8d86a6d21afa8e139ec045d
Fuma Nama <god63820869@gmail.com>
2024-12-26 21:25:30 +0800

    enable i18n & format

---
+++ app/[lang]/docs/layout.tsx
@@ -0,0 +1,18 @@
+import { DocsLayout } from "fumadocs-ui/layouts/docs";
+import type { ReactNode } from "react";
+import { baseOptions } from "@/app/layout.config";
+import { source } from "@/lib/source";
+
+export default async function Layout({
+  params,
+  children,
+}: {
+  params: Promise<{ lang: string }>;
+  children: ReactNode;
+}) {
+  return (
+    <DocsLayout tree={source.pageTree[(await params).lang]} {...baseOptions}>
+      {children}
+    </DocsLayout>
+  );
+}
